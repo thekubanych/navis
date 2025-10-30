0248044e31695f7d7d4730ed289dd5f5546ea5ec
thekubanych <kubanychmuhtarov>
2025-10-30 16:52:32 +0600

    comit for render

--- online_project/settings.py
+++ online_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,17 +155,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
 
 # Папка для команды collectstatic
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Django будет собирать туда все статические файлы
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Дополнительные папки со статикой, если есть
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
-# Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
