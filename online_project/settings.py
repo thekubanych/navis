4644bedd7325aaeaac821d39b780d11bdf36be99
thekubanych <kubanychmuhtarov>
2025-11-04 18:04:56 +0600

    for server

--- online_project/settings.py
+++ online_project/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4&3y#pb_)pey8z1402vpta_hyvyisc@@^p=bq*&+_8-m2mo=f%'
 
 DEBUG = False  # ← выключи debug на Render
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'navis-qvqs.onrender.com']
+ALLOWED_HOSTS = ["navis-qvqs.onrender.com", "localhost", "127.0.0.1"]
 
 # CKEditor
 CKEDITOR_UPLOAD_PATH = 'uploads/'
